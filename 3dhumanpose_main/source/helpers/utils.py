450d794b9e1f543b2b82011002504af9d655acbc
braunjon <25931090+braunjon@users.noreply.github.com>
2021-05-09 13:17:01 +0200

    Fix: lib import error

--- 3dhumanpose_main/source/helpers/utils.py
+++ 3dhumanpose_main/source/helpers/utils.py
@@ -7,39 +7,40 @@ import numpy as np
 import torch
 import torch.optim as optim
 
-from lib.core.config import get_model_name
 
-
-def create_logger(cfg, cfg_name, phase='train'):
-    root_output_dir = Path(cfg.OUTPUT_DIR)
-    # set up logger
-    if not root_output_dir.exists():
-        print('=> creating {}'.format(root_output_dir))
-        root_output_dir.mkdir()
-
-    dataset = cfg.DATASET.DATASET + '_' + cfg.DATASET.HYBRID_JOINTS_TYPE \
-        if cfg.DATASET.HYBRID_JOINTS_TYPE else cfg.DATASET.DATASET
-    dataset = dataset.replace(':', '_')
-    model, _ = get_model_name(cfg)
-    cfg_name = os.path.basename(cfg_name).split('.')[0]
-
-    final_output_dir = root_output_dir / dataset / model / cfg.EXP_NAME  # cfg_name
-
-    print('=> creating {}'.format(final_output_dir))
-    final_output_dir.mkdir(parents=True, exist_ok=True)
-
-    time_str = time.strftime('%Y-%m-%d-%H-%M')
-    log_file = '{}_{}_{}.log'.format(cfg_name, time_str, phase)
-    final_log_file = final_output_dir / log_file
-    head = '%(asctime)-15s %(message)s'
-    logging.basicConfig(filename=str(final_log_file),
-                        format=head)
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    console = logging.StreamHandler()
-    logging.getLogger('').addHandler(console)
-
-    return logger, str(final_output_dir)
+# from lib.core.config import get_model_name
+#
+#
+# def create_logger(cfg, cfg_name, phase='train'):
+#     root_output_dir = Path(cfg.OUTPUT_DIR)
+#     # set up logger
+#     if not root_output_dir.exists():
+#         print('=> creating {}'.format(root_output_dir))
+#         root_output_dir.mkdir()
+#
+#     dataset = cfg.DATASET.DATASET + '_' + cfg.DATASET.HYBRID_JOINTS_TYPE \
+#         if cfg.DATASET.HYBRID_JOINTS_TYPE else cfg.DATASET.DATASET
+#     dataset = dataset.replace(':', '_')
+#     model, _ = get_model_name(cfg)
+#     cfg_name = os.path.basename(cfg_name).split('.')[0]
+#
+#     final_output_dir = root_output_dir / dataset / model / cfg.EXP_NAME  # cfg_name
+#
+#     print('=> creating {}'.format(final_output_dir))
+#     final_output_dir.mkdir(parents=True, exist_ok=True)
+#
+#     time_str = time.strftime('%Y-%m-%d-%H-%M')
+#     log_file = '{}_{}_{}.log'.format(cfg_name, time_str, phase)
+#     final_log_file = final_output_dir / log_file
+#     head = '%(asctime)-15s %(message)s'
+#     logging.basicConfig(filename=str(final_log_file),
+#                         format=head)
+#     logger = logging.getLogger()
+#     logger.setLevel(logging.INFO)
+#     console = logging.StreamHandler()
+#     logging.getLogger('').addHandler(console)
+#
+#     return logger, str(final_output_dir)
 
 
 def get_optimizer(cfg, model, is_disc=False):
