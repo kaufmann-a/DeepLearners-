57c0503948fc1c4729c3adc5d39c3c0f3d7358d1
braunjon <25931090+braunjon@users.noreply.github.com>
2021-05-09 17:22:37 +0200

    Make integral-humane-pose networks selectable

--- 3dhumanpose_main/source/models/modelfactory.py
+++ 3dhumanpose_main/source/models/modelfactory.py
@@ -16,11 +16,13 @@ from source.configuration import Configuration
 from source.exceptions.configurationerror import ConfigurationError
 from source.models.basemodel import BaseModel
 from source.logcreator.logcreator import Logcreator
+from source.models.blocks import resnet_pose, resnet_direct_regression
 
 modules = glob.glob(dirname(__file__) + "/*.py")
 for module in [basename(f)[:-3] for f in modules if
                isfile(f) and not f.endswith('__init__.py') and not f == "modelfactory"]:
     __import__("source.models." + module)
+
 
 class ModelFactory(object):
 
@@ -33,4 +35,24 @@ class ModelFactory(object):
             model = [m(model_config) for m in all_models if m.name.lower() == model_config.name.lower()]
             if model and len(model) > 0:
                 return model[0]
-        raise ConfigurationError('training.model.name')
+        # raise ConfigurationError('training.model.name')
+
+        # TODO Maybe refactor to only use one method to get models
+        return getattr(ModelFactory, model_config.name)(ModelFactory, model_config)
+
+    @staticmethod
+    def ResPoseNet_DeconvHead(self, cfg):
+        default_config = resnet_pose.get_default_network_config()  # TODO use parameters from configuration file
+        return resnet_pose.get_pose_net(default_config, cfg.num_joints)
+
+    @staticmethod
+    def ResPoseNet_Regression(self, cfg):
+        default_config = resnet_direct_regression.get_default_network_config()  # TODO use parameters from configuration file
+        return resnet_direct_regression.get_pose_net(default_config, cfg.num_joints)
+
+    @staticmethod
+    def get_members():
+        return {
+            'ResPoseNet_DeconvHead': ModelFactory.ResPoseNet_DeconvHead,
+            'ResPoseNet_Regression': ModelFactory.ResPoseNet_Regression,
+        }
