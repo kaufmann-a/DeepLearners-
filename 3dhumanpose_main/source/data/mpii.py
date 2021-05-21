b5209b70b5d7e3323ead03be5492a4c634fe2127
braunjon <25931090+braunjon@users.noreply.github.com>
2021-05-21 15:47:36 +0200

    mpii updates

--- 3dhumanpose_main/source/data/mpii.py
+++ 3dhumanpose_main/source/data/mpii.py
@@ -11,6 +11,29 @@ from source.logcreator.logcreator import Logcreator
 MPII_PARENT_IDS = [1, 2, 6, 6, 3, 4, 6, 6, 7, 8, 11, 12, 7, 7, 13, 14]
 MPII_FLIP_PAIRS = [[0, 5], [1, 4], [2, 3], [10, 15], [11, 14], [12, 13]]
 
+s_mpii_2_hm36_jt = [6, 2, 1, 0, 3, 4, 5, -1, 8, -1, 9, 13, 14, 15, 12, 11, 10, 7]
+s_36_jt_num = 18
+
+
+def from_mpii_to_hm36_single(pose, pose_vis):
+    res_jts = np.zeros((s_36_jt_num, 3), dtype=np.float)
+    res_vis = np.zeros((s_36_jt_num, 3), dtype=np.float)
+
+    for i in range(0, s_36_jt_num):
+        id1 = i
+        id2 = s_mpii_2_hm36_jt[i]
+        if id2 >= 0:
+            res_jts[id1] = pose[id2].copy()
+            res_vis[id1] = pose_vis[id2].copy()
+
+    return res_jts.copy(), res_vis.copy()
+
+
+def from_mpii_to_hm36(db):
+    for n_sample in range(0, len(db)):
+        res_jts, res_vis = from_mpii_to_hm36_single(db[n_sample]['joints_3d'], db[n_sample]['joints_3d_vis'])
+        db[n_sample]['joints_3d'] = res_jts
+        db[n_sample]['joints_3d_vis'] = res_vis
 
 class MPIIDataset(JointDataset):
     name = 'mpii'
@@ -36,27 +59,32 @@ class MPIIDataset(JointDataset):
     def get_data(self, the_db):
         image_file = os.path.join(self.root, "images", the_db['image'])
 
-        center_x = the_db['center'][0]
-        center_y = the_db['center'][1]
+        if 'joints_3d_vis' in the_db.keys() and 'joints_3d' in the_db.keys():
+            joints = the_db['joints_3d'].copy()
+            joints_vis = the_db['joints_3d_vis'].copy()
+            joints_vis[:, 2] *= self.cfg_general.z_weight  # multiply the z axes of the visibility with z_weight
+        else:
+            joints = joints_vis = None
 
         # TODO add image transformations similar to get_single_patch_sample
         #  maybe use this as inspiration: https://github.com/yihui-he/epipolar-transformers/tree/4da5cbca762aef6a89d37f889789f772b87d2688/data/datasets
-        width = None
-        height = None
+        width = the_db['width']
+        height = the_db['height']
+
         img_patch, label, label_weight = get_single_patch_sample(image_file,
-                                                                 center_x, center_y,
+                                                                 the_db['center_x'], the_db['center_y'],
                                                                  width, height,
                                                                  self.patch_width, self.patch_height,
                                                                  self.rect_3d_width, self.rect_3d_height,
                                                                  self.mean, self.std, self.label_func,
-                                                                 joints=the_db['joints'],
-                                                                 joints_vis=the_db['joints_vis'], )
+                                                                 joints=joints,
+                                                                 joints_vis=joints_vis, )
         # DEBUG=self.cfg_general.DEBUG.DEBUG)  # TODO add debug parameter
 
         meta = {
             'image': image_file,
-            'center_x': center_x,
-            'center_y': center_y,
+            'center_x': the_db['center_x'],
+            'center_y': the_db['center_y'],
             'width': width,
             'height': height,
             'R': np.zeros((3, 3), dtype=np.float64),
@@ -72,6 +100,9 @@ class MPIIDataset(JointDataset):
         """
         Loads all images according to the chosen image set.
 
+        Based on https://github.com/yihui-he/epipolar-transformers/blob/4da5cbca762aef6a89d37f889789f772b87d2688/data/datasets/mpii.py
+             and https://github.com/JimmySuen/integral-human-pose/blob/master/pytorch_projects/common_pytorch/dataset/mpii.py
+
         :returns: the ground truth data base
         """
         # create train/val split for MPII
@@ -81,11 +112,63 @@ class MPIIDataset(JointDataset):
         gt_db = []
         for idx in range(len(annotations)):
             a = annotations[idx]
-            # TODO Maybe we want to do some preprocessing here? -> otherwise remove loop
-            gt_db.append(a)
+
+            # convert center and scale to 2d arrays representing the x and y axes
+            c = np.array(a['center'], dtype=np.float)
+            s = np.array([a['scale'], a['scale']], dtype=np.float)  # 2d array with individual scale for x and y
+
+            # TODO Ask on piazza if the following argumentations are already done to the mpii data that was
+            #  supplied by eth
+
+            # we adjust the center and scale slightly to avoid cropping of limbs
+            # (this is common practice in multiple git repos)
+            if c[0] != -1:
+                c[1] = c[1] + 15 * s[1]  # adjust the y center coordinate
+                s = s * 1.25  # increase the scaling
+
+            # MPII uses matlab format, index is based 1 -> convert it to 0-based index
+            assert c[0] >= 1  # checks for matlab format?
+            c = c - 1
+
+            width = s[0]
+            height = s[1]
+
+            width = width * 1.25 * self.pixel_std
+            height = height * 1.25 * self.pixel_std
+
+            if width / height >= 1.0 * self.patch_width / self.patch_height:
+                width = 1.0 * height * self.patch_width / self.patch_height
+            else:
+                assert 0, "Error. Invalid patch width and height"
+
+            # joints and vis
+            joints_3d = np.zeros((self.num_joints, 3), dtype=np.float)
+            joints_3d_vis = np.zeros((self.num_joints, 3), dtype=np.float)
+            if self.image_set != 'test':
+                jts = np.array(a['joints'])
+                jts[:, 0:2] = jts[:, 0:2] - 1
+                jts_vis = np.array(a['joints_vis'])
+                assert len(jts) == self.num_joints, 'joint num diff: {} vs {}'.format(len(jts), self.num_joints)
+                joints_3d[:, 0:2] = jts[:, 0:2]
+                joints_3d_vis[:, 0] = jts_vis[:]
+                joints_3d_vis[:, 1] = jts_vis[:]
+
+            gt_db.append({
+                'image': a['image'],
+                'center_x': c[0],
+                'center_y': c[1],
+                'width': width,
+                'height': height,
+                'flip_pairs': self.flip_pairs,
+                'parent_ids': self.parent_ids,
+                'joints_3d': joints_3d,
+                'joints_3d_vis': joints_3d_vis,
+            })
 
         # update the length of the data base
         self.db_length = len(gt_db)
+
+        from_mpii_to_hm36(gt_db)
 
         return gt_db
 
