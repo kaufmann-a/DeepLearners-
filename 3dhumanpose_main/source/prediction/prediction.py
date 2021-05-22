014b9eb3ff85611a029fa1a01e1fb887c2dc92bf
braunjon <25931090+braunjon@users.noreply.github.com>
2021-05-22 13:30:18 +0200

    Quick fix for inference

--- 3dhumanpose_main/source/prediction/prediction.py
+++ 3dhumanpose_main/source/prediction/prediction.py
@@ -15,6 +15,18 @@ from source.configuration import Configuration
 from source.data.datasetfactory import DataSetFactory
 
 
+class AttrWrapper(object):
+    """
+    https://stackoverflow.com/questions/6082625/python-dynamically-add-attributes-to-new-style-class-obj
+    """
+
+    def __init__(self, wrapped):
+        self._wrapped = wrapped
+
+    def __getattr__(self, n):
+        return getattr(self._wrapped, n)
+
+
 class Prediction(object):
 
     def __init__(self, engine, device):
@@ -26,11 +38,28 @@ class Prediction(object):
         self.engine = engine
         self.device = device
 
+    def get_dataset_config(self):
+        """
+        TODO Very ugly; Probably better to ad a parameter for the test set name or ...?
+
+        Forces dataset to be "h36m" and the attribute h36m_params.val_set to be "test".
+
+        Returns: data_collection config
+
+        """
+        dataset_config = Configuration.get('data_collection')
+
+        cfg = AttrWrapper(dataset_config)
+        cfg.dataset = ['h36m']
+        cfg.h36m_params = AttrWrapper(dataset_config.h36m_params)
+        cfg.h36m_params.val_set = "test"
+
+        return cfg
+
     def predict(self):
-        test_dataset = DataSetFactory.load(Configuration.get('data_collection'),
-                                           Configuration.get_path('data_collection.folder'),
-                                           image_set="test",
-                                           is_train=False)
+        cfg = self.get_dataset_config()
+
+        test_dataset = DataSetFactory.load(cfg, is_train=False)
 
         test_loader = torch.utils.data.DataLoader(
             test_dataset,
