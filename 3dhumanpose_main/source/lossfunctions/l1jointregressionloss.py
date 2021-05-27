558dfaef3eba9c46c524fc36a27d6f6638d2dc25
braunjon <25931090+braunjon@users.noreply.github.com>
2021-05-27 11:30:46 +0200

    Refactored l1 loss & Added average for regression loss

--- 3dhumanpose_main/source/lossfunctions/l1jointregressionloss.py
+++ 3dhumanpose_main/source/lossfunctions/l1jointregressionloss.py
@@ -1,8 +1,19 @@
 import torch
-from torch.nn import functional as F
-import numpy as np
 import torch.nn as nn
-import math
+
+
+def weighted_l1_loss(input, target, weights, size_average, norm=False):
+    if norm:
+        input = input / torch.norm(input, 1)
+        target = target / torch.norm(target, 1)
+
+    out = torch.abs(input - target)
+    out = out * weights
+    if size_average:
+        num_valid = weights.byte().any(dim=1).float().sum()
+        return out.sum() / num_valid
+    else:
+        return out.sum()
 
 
 class L1JointRegressionLoss_eth_code(nn.Module):
@@ -13,49 +24,29 @@ class L1JointRegressionLoss_eth_code(nn.Module):
         self.num_joints = num_joints
         self.norm = norm
 
-    def weighted_l1_loss(self, input, target, weights, size_average, norm=False):
-
-        if norm:
-            input = input / torch.norm(input, 1)
-            target = target / torch.norm(target, 1)
-
-        out = torch.abs(input - target)
-        out = out * weights
-        if size_average:
-            num_valid = weights.byte().any(dim=1).float().sum()
-            return out.sum() / num_valid
-        else:
-            return out.sum()
-
     def _assert_no_grad(self, tensor):
         assert not tensor.requires_grad, \
             "nn criterions don't compute the gradient w.r.t. targets - please " \
             "mark these tensors as not requiring gradients"
 
-    def forward(self, preds, *args):
-        gt_joints = args[0]
-        gt_joints_vis = args[1]
-
+    def forward(self, preds, gt_joints, gt_joints_vis):
         pred_jts = preds.reshape((preds.shape[0], self.num_joints * 3))
 
         self._assert_no_grad(gt_joints)
         self._assert_no_grad(gt_joints_vis)
-        return self.weighted_l1_loss(pred_jts, gt_joints, gt_joints_vis, self.size_average, self.norm)
+        return weighted_l1_loss(pred_jts, gt_joints, gt_joints_vis, self.size_average, self.norm)
 
 
 class L1JointRegressionLoss(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, size_average=True):
+        super(L1JointRegressionLoss, self).__init__()
+        self.size_average = size_average
 
-    @staticmethod
-    def regression_loss_2d(pred_joints, gt_joints, gt_joints_vis):
-        error = abs(pred_joints[:, :2] - gt_joints) * gt_joints_vis.unsqueeze(1)
-        return torch.sum(error)
+    def regression_loss_2d(self, pred_joints, gt_joints, gt_joints_vis):
+        return weighted_l1_loss(pred_joints[:, :2], gt_joints, gt_joints_vis.unsqueeze(1), self.size_average)
 
-    @staticmethod
-    def regression_loss_3d(pred_joints, gt_joints, gt_joints_vis):
-        error = abs(pred_joints - gt_joints) * gt_joints_vis.unsqueeze(1)
-        return torch.sum(error)
+    def regression_loss_3d(self, pred_joints, gt_joints, gt_joints_vis):
+        return weighted_l1_loss(pred_joints, gt_joints, gt_joints_vis.unsqueeze(1), self.size_average)
 
     def forward(self, preds, batch_joints, batch_joints_vis):
         N, J, _ = preds.shape
